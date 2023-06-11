5b141ae746928cb2b2b2d239da4eb0efa5b32aef
Mingkuan Zhao <91729156+Harry-Miral@users.noreply.github.com>
2023-06-11 14:28:10 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import videopose_PSTMO
 import npload
 import angel
 
-PSTMO_path = 'normal/zmk gait.mp4'# Provide your video path here
+PSTMO_path = 'normal/example.mp4'# Provide your video path here
 videopose_PSTMO.inference_video(PSTMO_path, 'alpha_pose')
-npload.npload_point('zmk gait')# Please cooperate with npload.py to modify the storage path of your joint position time series data
-angel.angel_all('zmk gait','normal/')# Your angular time series data goes here
+npload.npload_point('example')# Please cooperate with npload.py to modify the storage path of your joint position time series data
+angel.angel_all('example','normal/')# Your angular time series data goes here
 
